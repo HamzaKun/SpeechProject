d9bcee28e6fdcc7b1324e9ee6f416ab155ca929d
HamzaKun <hamzakasry@gmail.com>
2016-01-02 23:30:22 +0000

    Some changes in the sturcture of the project
    -The VRecognizer_Test is not used
    -Organised the Vrecogniser, and the Synthesis classes to fit the new
    changes
    -Added the new commponents in hte GUI class, aside from the action
    listener to start the recognition and synthesis

--- src/ma/ensa/Synthesis.java
+++ src/ma/ensa/Synthesis.java
@@ -5,35 +5,51 @@ import com.sun.speech.freetts.VoiceManager;
 
 public class Synthesis {
 	VoiceManager freettsVM;
-    Voice freettsVoice;
-    String words;
-    
-    public Synthesis(String speech){
-    	 // Set propreties
-        //System.setProperty("mbrola.base", "C:/Users/hamza/Desktop/SpeechDocuments/Syntesis/mbrola");
-        ////
-    	
-    	System.setProperty("mbrola.base","C:/Users/ibouig/Documents/GitHub/SpeechProject/mbrola");
+	Voice freettsVoice;
+	String words;
+	/**
+	 * Create a Synthesizable object with
+	 * with the the default voice mbrola_us1
+	 * @param speech
+	 */
+	public Synthesis(String speech){
+		// Set properties
+		System.setProperty("mbrola.base", "./mbrola");
 
-        freettsVM = VoiceManager.getInstance();
+		freettsVM = VoiceManager.getInstance();
 
-        // Simply change to MBROLA voice
-        freettsVoice = freettsVM.getVoice("mbrola_us1");
-        
+		// Simply change to MBROLA voice
+		freettsVoice = freettsVM.getVoice("mbrola_us1");
 
-        // Allocate your chosen voice
-        freettsVoice.allocate();
-        //To speak
-        words = new String(speech);
-        //freettsVoice.speak(speech);
-    }
-    
-    public void setWords(String speech){
-    	words = new String(speech);
-    }
-    
-    public void SayIt(){
-    	freettsVoice.speak(words);
-    }
+		// Allocate your chosen voice
+		freettsVoice.allocate();
+		
+		//Set the word to be spoken
+		words = new String(speech);
+	}
+
+	/**
+	 * To change the words to be spoken
+	 * @param speech
+	 */
+	public void setWords(String speech){
+		words = new String(speech);
+	}
+
+	/**
+	 * Change the voice, and reallocate it 
+	 * @param name
+	 */
+	public void setVoice(String name){
+		freettsVoice = freettsVM.getVoice(name);
+		freettsVoice.allocate();
+	}
+	
+	/**
+	 * To start speaking
+	 */
+	public void SayIt(){
+		freettsVoice.speak(words);
+	}
 
 }
--- src/ma/ensa/VRecognizer.java
+++ src/ma/ensa/VRecognizer.java
@@ -14,80 +14,114 @@ import java.net.URL;
 
 
 public class VRecognizer {
+
+	static ConfigurationManager cm;
+	private String resultText;
+	private Recognizer recognizer;
+	private Microphone microphone;
+
+
+	/**
+	 * The Constructor is only for allocating the necessary ressources
+	 * the recognizer, microphone and the grammar
+	 * @param args
+	 */
+
+	VRecognizer(String[] args){
+		try {
+			URL url;
+			if (args.length > 0) {
+				url = new File(args[0]).toURI().toURL();
+			} else {
+				url = VRecognizer.class.getResource("grammar.config.xml");
+			}
+
+			System.out.println("Loading...");
+
+			cm = new ConfigurationManager(url);
+			recognizer = (Recognizer) cm.lookup("recognizer");
+			microphone = (Microphone) cm.lookup("microphone");
+		}
+		catch (IOException e) {
+			System.err.println("Problem when loading Reco: " + e);
+			e.printStackTrace();
+		} 
+		catch (PropertyException e) {
+			System.err.println("Problem configuring Reco: " + e);
+			e.printStackTrace();
+		} 
+
+	}
+
+	/**
+	 * StartRec is the function that starts the recognition
+	 */
+	void StartRec(){
+		/* allocate the resource necessary for the recognizer */
+		recognizer.allocate();
+
+		/* the microphone will keep recording until the program exits */
+		if (microphone.startRecording()){
+			System.out.println("Speak: ");
+
+			while (true) {
+				System.out.println("Start speaking using hello grammar");
+				Result result = recognizer.recognize();
+				Synthesis s ; 
+
+				if (result != null) {
+
+					resultText = result.getBestFinalResultNoFiller();
+					System.out.println("You said: " + resultText + "\n");
+					s= new Synthesis( resultText);
+					s.SayIt();
+					if(isCommand(resultText)){
+						try{
+							Process exec = new ProcessBuilder("CMD", "/C", getCommand(resultText)).start();
+						}catch(Exception e){
+							System.out.println("Problem while trying to run a command");
+							e.printStackTrace();
+						}
+						//Here we can add the microphone.stopRecognition if necessary
+						microphone.clear();
+					}
+				} else {
+					System.out.println("I can't hear what you said.\n");
+					s= new Synthesis("I can't hear what you said");
+					s.SayIt();
+				}
+			}
+		}else{
+			System.out.println("Cannot start microphone.");
+			recognizer.deallocate();
+			System.exit(1);
+		}
+	}
+
+
+	/**
+	 * A function that checks if the result of the recognition is a command
+	 * as open, close or some others
+	 * @param arg
+	 * @return
+	 */
+	public boolean isCommand(String arg){
+		boolean b = true;
+		return b;
+	}
 	
-    static ConfigurationManager cm;
-    
-    
-   
-    public static void main(String[] args) {
-        try {
-            	URL url;
-            	if (args.length > 0) {
-            		url = new File(args[0]).toURI().toURL();
-            	} else {
-            		url = VRecognizer.class.getResource("grammar.config.xml");
-            	}
-            
-            	System.out.println("Loading...");
-            
-            	cm = new ConfigurationManager(url);
-            	Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-            	Microphone microphone = (Microphone) cm.lookup("microphone");
+	/**
+	 * Used to get the command
+	 * the String that will be added to the ProcessBuilder
+	 * @param arg
+	 * @return
+	 */
+	public String getCommand(String arg){
+		String cmd = new String();
+		return cmd;
+	}
+	public static void main(String[] args) {
 
-            	/* allocate the resource necessary for the recognizer */
-            	recognizer.allocate();
-
-            	/* the microphone will keep recording until the program exits */
-            	if (microphone.startRecording()) {
-            		System.out.println("Speak: ");
-
-            		while (true) {
-            			System.out.println("Start speaking using hello grammar");
-            			Result result = recognizer.recognize();
-            			Synthesis s ; 
-        				
-            			if (result != null) {
-            				String resultText = result.getBestFinalResultNoFiller();
-            				s= new Synthesis( resultText);
-            				s.SayIt();
-            				System.out.println("You said: " + resultText + "\n");
-            			} else {
-            				
-            				s= new Synthesis("I can't hear what you said");
-            				s.SayIt();
-            				System.out.println("I can't hear what you said.\n");
-            			}
-
-                    
-            		}
-		 
-            	} else {
-            		System.out.println("Cannot start microphone.");
-            		recognizer.deallocate();
-            		System.exit(1);
-            	}
-        	} 
-        
-        	catch (IOException e) {
-        		System.err.println("Problem when loading Reco: " + e);
-        		e.printStackTrace();
-        	} 
-        	catch (PropertyException e) {
-        		System.err.println("Problem configuring Reco: " + e);
-        		e.printStackTrace();
-        	} 
-        	
-    }
-
-    static void swapGrammar(String newGrammarName) 
-            throws PropertyException, InstantiationException, IOException {
-        System.out.println("Swapping to grammar " + newGrammarName);
-        Linguist linguist = (Linguist) cm.lookup("flatLinguist");
-        linguist.deallocate();
-
-        //cm.setProperty("jsgfGrammar", "grammarName", newGrammarName);
-        linguist.allocate();
-    }
-
+	}
 
 }
